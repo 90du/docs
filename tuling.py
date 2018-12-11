fea0383dbf265ea8b956f9b9a5288c1eb2c657be
Rekols <rekols@foxmail.com>
2018-12-11 19:01:44 +0800

    Create tuling.py

---
+++ tuling.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import re
+
+def answer(ask):
+    url = 'http://openapi.tuling123.com/openapi/api/v2'
+    body = {
+        "reqType":0,
+        "perception": {
+            "inputText": {
+                "text": ""
+            }
+        },
+        "userInfo": {
+            "apiKey": "2bb504a011cd4350bdde0149293e28c2",
+            "userId": "364434"
+        }
+    }
+    body['perception']['inputText']['text'] = ask
+    data = json.dumps(body)
+    response = requests.post(url, data = data)
+    retext = response.text
+    answ = re.compile('{.*?results":.*?values.*?text":"(.*?)"}', re.S)
+    text = re.findall(answ, retext)
+    text = str(text[0])
+    return text
